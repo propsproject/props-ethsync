f1d08fdd6582d02bddd31c781553a83d6d129ea5
jon <jon@younow.com>
2019-09-18 10:29:42 -0400

    removed @younow/lib-env

--- lib/config.ts
+++ lib/config.ts
@@ -1,4 +1,4 @@
-import { Config, settings as coreSettings } from '@younow/lib-env';
+import { Config, settings as coreSettings } from 'props-lib-env';
 import settings from './settings/index';
 
 const config = new Config();
