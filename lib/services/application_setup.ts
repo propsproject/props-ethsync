706c80acc783d1c6cd092b8d0bd288e3be38a5fa
jon <jon@younow.com>
2020-02-12 08:51:31 -0500

    updated submit gas and gas price default and being able to receive from env variables

--- lib/services/application_setup.ts
+++ lib/services/application_setup.ts
@@ -22,6 +22,9 @@ export default class ApplicationSetup {
   async setupViaMultiSig(pk:string, _multiSigAddress:string, _name:string, _rewardsAddress:string, _sidechainAddress:string) {
 
     let provider;
+    if (process.env.ETHEREUM_URL_ETHSYNC.length > 0) {
+      config.settings.ethereum.uri = process.env.ETHEREUM_URL_ETHSYNC;
+    }
     if (config.settings.ethereum.localhost_test_contract.length > 0) {
       provider = new PrivateKeyProvider(pk, 'http://localhost:8545');
     } else {
@@ -31,15 +34,21 @@ export default class ApplicationSetup {
     const web3 = new Web3(provider);
     const account = web3.eth.accounts.privateKeyToAccount(`0x${pk}`);
     console.log(account.address);
-    const tokenContractAddress = config.settings.ethereum.localhost_test_contract.length 
+    let tokenContractAddress = config.settings.ethereum.localhost_test_contract.length 
     > 0 ? config.settings.ethereum.localhost_test_contract : config.settings.ethereum.token_address;
+    if (process.env.PROPS_TOKEN_CONTRACT_ADDRESS.length > 0) {
+      tokenContractAddress = process.env.PROPS_TOKEN_CONTRACT_ADDRESS;
+    }
     const TokenContract = new web3.eth.Contract(JSON.parse(Utils.abi()), tokenContractAddress);
 
     const multisigWalletABI = require('./MultiSigWallet.json');
     const multiSigContractInstance = new web3.eth.Contract(multisigWalletABI.abi, _multiSigAddress);
     const encodedData = await TokenContract.methods.updateEntity(0, web3.utils.asciiToHex(_name), _rewardsAddress, _sidechainAddress,
     ).encodeABI();
-
+    console.log(`config.settings.ethereum.uri=${config.settings.ethereum.uri}`);
+    console.log(`tokenContractAddress=${tokenContractAddress}`);
+    console.log(`config.settings.ethereum.entity_setup_multisig_gas=${config.settings.ethereum.entity_setup_multisig_gas}`);
+    console.log(`this.web3.utils.toWei(config.settings.ethereum.gas_price, 'gwei')=${this.web3.utils.toWei(config.settings.ethereum.gas_price, 'gwei')}`);
     try {
       await multiSigContractInstance.methods.submitTransaction(
         tokenContractAddress,
